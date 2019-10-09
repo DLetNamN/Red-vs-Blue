995785a773daa738be7e4a9d907da29f6607cf05
DLetNamN <wissam.hattab@elev.ga.lbs.se>
2019-10-09 10:45:10 +0200

    created movement according to the video, started editing the values to fit the ship size and type. Keep in mind to recheck the video later.

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [Header("Inputs")]
+    public string player;
+
+    [Header("Player Stats")]
+    public float thrustSpeed;
+    public float turnSpeed;
+    public float health;
+    public float bulletDmg;
+    public float bulletSpeed;
+    public float bulletKnockback;
+    public float cooldownTimer;
+
+    [Header("Game Components")]
+    public Rigidbody2D rbody;
+    public Transform shootPoint;
+
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+
+        thrustSpeed = 2;
+
+        turnSpeed = 2;
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Jump"))
+        {
+            rbody.AddForce(transform.up * thrustSpeed);
+        }
+
+        Vector3 lookVec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 4096);
+        
+        if(lookVec.x != 0 || lookVec.y != 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lookVec, Vector3.back);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+        }
+    }   
+}
